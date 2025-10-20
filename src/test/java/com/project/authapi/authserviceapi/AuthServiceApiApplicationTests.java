5d1104f5988ba7daec62ac9c8b2e682e1f9f6577
Jakub <jakubfrolow@gmail.com>
2025-10-20 20:48:39 +0200

    Commit 6 - Tests

--- src/test/java/com/project/authapi/authserviceapi/AuthServiceApiApplicationTests.java
+++ src/test/java/com/project/authapi/authserviceapi/AuthServiceApiApplicationTests.java
@@ -8,6 +8,7 @@ class AuthServiceApiApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ src/test/java/com/project/authapi/authserviceapi/service/JwtServiceTest.java
@@ -0,0 +1,63 @@
+package com.project.authapi.authserviceapi.service;
+
+
+import com.project.authapi.authserviceapi.entity.Role;
+import com.project.authapi.authserviceapi.entity.User;
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = JwtService.class, properties = {
+        "jwt.secret=test-secret-key-which-is-long-enaugh",
+        "jwt.expiration-ms=3600000"})
+
+public class JwtServiceTest {
+    @Autowired
+    private JwtService jwtService;
+    private User testUser;
+
+    @BeforeEach // Building|reseting testUser for tests.
+    public void setup() {
+        testUser = User.builder()
+                .username("TestUser")
+                .password("TestPassword")
+                .email("TestMail@mail.com")
+                .role(Role.USER)
+                .enabled(true)
+                .build();
+    }
+
+    @Test
+    void generateToken_shouldReturnToken() {
+        String token = jwtService.generateToken(testUser);
+        assertNotNull(token);
+        assertFalse(token.isEmpty());
+    }
+
+    @Test
+    void extractUsername_shouldReturnUsername() {
+        String token = jwtService.generateToken(testUser);
+        String username = jwtService.extractUsername(token);
+        assertNotNull(username);
+        assertEquals("TestUser", username);
+    }
+
+    @Test
+    void isTokenValid_shouldReturnTrue() {
+        String token = jwtService.generateToken(testUser);
+        String fakeToken = jwtService.generateToken(testUser) + "fakeToken";
+        boolean valid = jwtService.isTokenValid(token, testUser);
+        boolean invalid = jwtService.isTokenValid(fakeToken, testUser);
+        assertTrue(valid);
+        //assertFalse(invalid);
+    }
+
+
+
+
+}
